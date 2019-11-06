f3b4a655e4c699b4dcd04961784b2fe8a43ae968
Abhi <abhisheksubba88@gmail.com>
2019-11-06 11:28:53 +0530

    Broken HomePage into smaller components and finished styling the HomePage

---
+++ src/Pages/HomePage/HomePage.Component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./HomePage.Styles.scss"
+import DirectoryComponent from "../../Components/Directories/DirectoryComponent"
+
+export default function HomePage() {
+ return (
+  <div className="homepage">
+   <DirectoryComponent/>
+  </div>
+ )
+}
